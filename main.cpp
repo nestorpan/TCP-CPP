7cc45ea60448cb9b211f08454ad7dc95ef725280
Nestor <nestor.ariel.pan@gmail.com>
2019-09-14 14:59:27 -0300

    Inicial.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include "Fecha.h"
+
+using namespace std;
+
+
+int main()
+{
+    Fecha fDef;
+    Fecha fIngr;
+    
+  ///  cout << "Ingrese una fecha(d/m/a): ";
+    
+  ///  cin >> fIngr;
+	
+    filebuf fbIn;
+	fbIn.open("test.txt", ios::in);
+	istream archivoIn(&fbIn);
+	archivoIn >> fIngr;
+	fbIn.close();
+	
+    
+    int d, m, a;
+    
+    fIngr.getDMA(d, m, a);
+    
+    cout << "La fecha es " << d << '/' << m << '/' << a << endl;
+    
+    
+    Fecha fSuma = fIngr + 90;
+	
+	fSuma.getDMA(d, m, a);
+	
+  ///  cout << "La fecha suma es " << d << '/' << m << '/' << a << endl;
+    
+    cout << "La fecha suma es " << fSuma << endl;
+    
+    
+    filebuf fb;
+	fb.open ("test.txt", ios::out);
+	ostream archivo(&fb);
+	archivo << "La fecha suma es " << fSuma << endl;
+	fb.close();
+    
+    
+    
+    return 0;
+}
---
+++ src/Fecha.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include "Fecha.h"
+
+using namespace std;
+
+int Fecha::vcdm[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int Fecha::mda[2][13] =
+{
+	{0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365},
+	{0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366}
+};
+
+Fecha::Fecha()
+{
+	cout << "Constructor ejecutando." << endl;
+	diaRel = 1;
+}
+
+
+Fecha::Fecha(int d, int m, int a)
+{
+	setDMA(d, m, a);
+}
+
+
+Fecha Fecha::operator +(int dias) const
+{
+	Fecha suma(*this);
+	
+	suma.diaRel += dias;
+	
+	return suma;
+}
+
+
+void Fecha::getDMA(int& d, int& m, int& a) const
+{
+	int difAnios = diaRel / 365;
+	
+	int cantDiasAniosCompl = difAnios * 365 + difAnios / 4 - difAnios / 100 + difAnios / 400;
+	
+	while(cantDiasAniosCompl > diaRel)
+	{
+		difAnios--;
+		cantDiasAniosCompl = difAnios * 365 + difAnios / 4 - difAnios / 100 + difAnios / 400;
+	}
+	
+	a = 1601 + difAnios;
+	
+	int diaDelAnio = diaRel - cantDiasAniosCompl;
+	
+	int fila = this->esBisiesto(a)? 1 : 0;
+	
+	m = 1;
+	
+	while(diaDelAnio > mda[fila][m])
+		m++;
+	
+	d = diaDelAnio - mda[fila][m-1];
+}
+
+
+void Fecha::setDMA(int d, int m, int a)
+{
+	if(!esFechaValida(d, m, a))
+		throw 1;
+	
+	int difAnios = a - 1601;
+	
+	diaRel = difAnios * 365 + difAnios / 4 - difAnios / 100 + difAnios / 400 + diaDelAnio(d, m, a);
+}
+
+
+bool Fecha::esFechaValida(int d, int m, int a) const
+{
+	return 	a > 1600 &&
+			m >= 1 && m <= 12 &&
+			d >= 1 && d <= cantDiasMes(m, a);
+}
+
+
+int Fecha::cantDiasMes(int m, int a) const
+{
+	if(m == 2 && esBisiesto(a))
+		return 29;
+	
+	return vcdm[m-1];
+}
+
+
+bool Fecha::esBisiesto(int a) const
+{
+	return (a % 4 == 0 && a % 100 != 0) || a % 400 == 0;
+}
+
+
+int Fecha::diaDelAnio(int d, int m, int a) const
+{
+	return mda[esBisiesto(a)? 1 : 0][m - 1] + d;
+}
+
+
+ostream& operator <<(ostream& sal, const Fecha& f)
+{
+	int d, m, a;
+    
+    f.getDMA(d, m, a);
+    
+    sal << d << '/' << m << '/' << a;
+	
+	return sal;
+}
+
+
+istream& operator >>(istream& ent, Fecha& f)
+{
+	int d, m, a;
+	char c;
+	
+	ent >> d >> c >> m >> c >> a;
+	
+	f.setDMA(d, m, a);
+	
+	return ent;
+}
