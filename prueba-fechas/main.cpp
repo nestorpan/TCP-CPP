63757795cf3fc420e526a89b2d151bff7652bd75
guzmaneg <54598391+guzmaneg@users.noreply.github.com>
2020-09-12 19:46:09 -0300

    Commit clase 2020-09-12 T
    Commit clase 2020-09-12 T

--- prueba-fechas/main.cpp
+++ prueba-fechas/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <Fecha.h>
+#include <FechaException.h>
 
 using namespace std;
 
 
 int main()
 {
-	const Fecha f;
-	
-	Fecha sum = f.sumarDias(30);
-	
+//	const Fecha f;
+
+//	Fecha sum = f.sumarDias(30);
+
+//	cout << "Fecha " << endl;
+
+    try {
+        Fecha f(29, 2, 2021);
+    }
+    catch(FechaException& ex) {
+        cout << "Se ha producido un error ==> " << ex.getMensaje() << endl;
+        return -1;
+    }
+
+   cout << "Fecha OK" << endl;
+
     return 0;
 }
