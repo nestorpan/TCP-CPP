678553c77af45bb74a95b2d50d2d1a5a3a643ab6
guzmaneg <54598391+guzmaneg@users.noreply.github.com>
2020-10-10 15:33:23 -0300

    Clase 2020-10-10 - Cadenas - Concatenar con un solo +. Split.
    Clase 2020-10-10 - Cadenas - Concatenar con un solo +. Split.

--- include/Cadena.h
+++ include/Cadena.h
@@ -2,6 +2,7 @@
 #define CADENA_H
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,26 +15,34 @@ class Cadena
 
         static int cantCall;
 
+        static int contarDigitos(int d);
+
     public:
         Cadena();
         Cadena(const char* cad);
+        Cadena(char c);
+        Cadena(int d);
 
         Cadena(const Cadena& cadena); /// Constructor de copia
         ~Cadena(); /// Destructor
 
         Cadena& operator =(const Cadena& otra); /// Op asignación
-        Cadena operator +(const Cadena& otra) const;
+        ///Cadena operator +(const Cadena& otra) const;
+        Cadena& operator +=(const Cadena& otra);
         /// Cadena& operator +=(const Cadena& otra);
 
         int getCantCall();
 
-        char caracterEnPos(int pos) const;
-        int posCaracter(char c) const;
-        Cadena podar() const;
-        Cadena subcadena(int ini, int fin) const; /// "Hola que tal?".sucadena(0, 4) ==> "Hola"; (1, 3) ==> "ol"
-        bool contiene(const Cadena& cadABuscar) const;
-        Cadena reemplazar(const Cadena& cadABuscar, const Cadena& cadReempl) const;
+///        char caracterEnPos(int pos) const;
+        int posCaracter(char c, int ini = 0) const;
+///        Cadena podar() const;
+        Cadena subcadena(int ini, int fin) const;
+        Cadena subcadena(int ini) const;
+///        bool contiene(const Cadena& cadABuscar) const;
+///        Cadena reemplazar(const Cadena& cadABuscar, const Cadena& cadReempl) const;
         vector<Cadena> split(char separador) const; /// Hola, que tal? split(',') ==> vector = {"Hola", " que tal?"}
+
+        friend Cadena operator +(const Cadena& cad1, const Cadena& cad2);
 
         friend ostream& operator <<(ostream& sal, const Cadena& cadena);
         friend istream& operator >>(istream& ent, Cadena& cadena);
