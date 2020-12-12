711f343e2ab71a97bb1238c3a01039d392ff56a7
guzmaneg <54598391+guzmaneg@users.noreply.github.com>
2020-12-12 14:10:54 -0300

    Clase 2020-12-12 - Pre y Pos Incremento.
    Clase 2020-12-12 - Pre y Pos Incremento.

--- fecha/src/Fecha.cpp
+++ fecha/src/Fecha.cpp
@@ -37,7 +37,7 @@ void Fecha::setDMA(int dia, int mes, int anio)
 Fecha& Fecha::operator +=(int dias)
 {
 	this->diaRel += dias;
-	
+
 	return *this;
 }
 
@@ -45,9 +45,9 @@ Fecha& Fecha::operator +=(int dias)
 Fecha Fecha::operator +(int dias) const
 {
 	Fecha fSuma(*this);
-	
+
 	fSuma.diaRel += dias;
-	
+
 	return fSuma;
 }
 
@@ -72,7 +72,7 @@ bool Fecha::esFechaValida(int dia, int mes, int anio)
             if(dia >= 1 && dia <= cantDiasMes(mes, anio))
                 return true;
 	}
-	
+
 	return false;
 }
 
@@ -80,10 +80,10 @@ bool Fecha::esFechaValida(int dia, int mes, int anio)
 int Fecha::cantDiasMes(int mes, int anio)
 {
 	static const int vCantDias[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
 	if(mes == 2 && esBisiesto(anio))
 		return 29;
-	
+
 	return vCantDias[mes];
 }
 
@@ -92,14 +92,14 @@ void Fecha::getDMA(int& d, int& m, int& a) const
 {
 	int aniosComplCalc = this->diaRel / 365;
 	int diasAniosComplCalc;
-	
+
 	while((diasAniosComplCalc = diasAniosCompletos(aniosComplCalc)) >= this->diaRel)
 		aniosComplCalc--;
-	
+
 	a = ANIO_BASE + aniosComplCalc;
-	
+
 	int diaDelAnio = this->diaRel - diasAniosComplCalc;
-	
+
 	diaYMes(diaDelAnio, a, d, m);
 }
 
@@ -114,10 +114,10 @@ void Fecha::diaYMes(int diaDelAnio, int anio, int& dia, int& mes)
 {
 	mes = 1;
 	int filaAcum = esBisiesto(anio)? 1 : 0;
-	
+
 	while(mes <= 12 && diaDelAnio > acumDias[filaAcum][mes])
 		mes++;
-	
+
 	mes--;
 	dia = diaDelAnio - acumDias[filaAcum][mes];
 }
@@ -150,23 +150,36 @@ bool Fecha::operator >=(const Fecha& f2) const
 Fecha operator +(int dias, const Fecha& f)
 {
 	Fecha fSuma(f);
-	
+
 	fSuma.diaRel += dias;
-	
+
 	return fSuma;
 }
 
+Fecha& Fecha::operator ++()	/// Pre
+{
+	++this->diaRel;
+	return *this;
+}
+
+
+Fecha Fecha::operator ++(int) /// Pos
+{
+	Fecha temp(*this);
+	this->diaRel++;
+	return temp;
+}
 
 Fecha Fecha::hoy()
 {
 	time_t segs = time(NULL);
-	
+
 	struct tm* timeMachine = localtime(&segs);
-	
+
 	int dia = timeMachine->tm_mday;
 	int mes = timeMachine->tm_mon + 1;
 	int anio = timeMachine->tm_year + 1900;
-	
+
 	return Fecha(dia, mes, anio);
 }
 
@@ -175,7 +188,7 @@ ostream& operator <<(ostream& sal, const Fecha& f)
 {
 	int d, m, a;
 	f.getDMA(d, m, a);
-	
+
 	return sal << d << '/' << m << '/' << a;
 }
 
@@ -184,11 +197,11 @@ istream& operator >>(istream& ent, Fecha& f)
 {
 	int d, m, a;
 	char c;
-	
+
 	ent >> d >> c >> m >> c >> a;
-	
+
 	f.setDMA(d, m, a);
-	
+
 	return ent;
 }
 
--- prueba-fechas/main.cpp
+++ prueba-fechas/main.cpp
@@ -16,39 +16,39 @@ int main(int argc, char* argv[])
 	filebuf fbSal;
     fbSal.open(argv[ARG_TXT_SAL], ios::out);
     ostream salida(&fbSal);
-	
+
 	const Fecha f(12, 1, 2020);
-	
+
 	int dias = 30;
 	Fecha sum = dias + f;
 
 	cout << f << " + " << dias << " dias es: " << sum << endl;
 	salida << f << " + " << dias << " dias es: " << sum << endl;
-	
+
 	Fecha f2(12, 1, 2021);
-	
+
 //	int dif = f2 - f;
-	
+
 	cout << "Dif en días: " << f2 - f << endl;
 	salida << "Dif en días: " << f2 - f << endl;
-	
+
 	string sino;
-	
+
 	if(f >= f2)
 		sino = "no";
-	
+
 	cout << f << sino << " es menor que " << f2 << endl;
 	salida << f << sino << " es menor que " << f2 << endl;
-	
-	
+
+
 	Fecha fIngr;
 
 ///	cout << "Ingrese una fecha (d/m/a):" << endl;
-	
+
 	filebuf fbEnt;
     fbEnt.open(argv[ARG_TXT_ENT], ios::in);
     istream entrada(&fbEnt);
-	
+
 	try
 	{
 		entrada >> fIngr;
@@ -58,9 +58,17 @@ int main(int argc, char* argv[])
 		cout << "Hubo un error en el ingreso de la fecha: " << ex.getMensaje() << endl;
 		return ERR_INGR_FECHA;
 	}
-	
+
 	cout << "La fecha ingresada es: " << fIngr << endl;
-	
-	
+
+	cout << endl;
+	cout << "Pre y pos Incrementos:" << endl;
+
+	Fecha hoy = Fecha::hoy();
+
+	cout << hoy << endl;
+	cout << hoy++ << endl;
+	cout << ++hoy << endl;
+
     return 0;
 }
