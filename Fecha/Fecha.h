b501f04fde9816d961454d6f563edbce6a8b67b1
nestor <nestor.ariel.pan@gmail.com>
2022-04-30 12:16:19 -0300

    Clase 2022-04-30 - Clase Fecha - Dia rel a DMA - Op ++ - <<.

--- Fecha/Fecha.h
+++ Fecha/Fecha.h
@@ -1,21 +1,44 @@
 #ifndef FECHA_H
 #define FECHA_H
 
+#include <iostream>
+
+#define ANIO_BASE 1601
+
+using namespace std;
+
 
 class Fecha
 {
 private:
 	int diaRel;
 
+	static const int acumDiasMes[14];
+	static const int acumDiasMesBis[14];
+
+	static int diaDelAnio(int dia, int mes, int anio);
+	static bool esBisiesto(int anio);
+
 public:
 	Fecha();
 	Fecha(int dia, int mes, int anio);
 
-	Fecha sumarDias(int dias) const;
-	int difEnDias(const Fecha* otraFecha) const;
 	Fecha operator +(int dias) const;
-	int operator -(const Fecha* otraFecha) const; //const al final indica que no se modifica el objeto llamador
+	int operator -(const Fecha& otraFecha) const; //const al final indica que no se modifica el objeto llamador
+	Fecha& operator ++(); //preincremento ++fecha
+	Fecha operator ++(int);	//postincremento fecha++
+	void setDMA(int dia, int mes, int anio);
+	void getDMA(int& dia, int& mes, int& anio) const;
+	//TODO: Agregar métodos de comparación (< > <= >= == !=)
+	//TODO: Agregar -- Pre y Post
+	//TODO: Agregar += y -=
+	//TODO: Agregar - dias
+	//TODO: Investigar uso de cin
+	//TODO: operator istream >> 
 };
+
+
+ostream& operator <<(ostream& os, const Fecha& f);
 
 
 #endif // FECHA_H
--- TestFecha/main.cpp
+++ TestFecha/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <locale.h>
+#include <windows.h>
 #include "../Fecha/Fecha.h"
 
 using namespace std;
@@ -6,16 +8,24 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_CTYPE, "spanish");
+
     Fecha fDefault;
-    Fecha fecha1(1, 1, 2000);
-    Fecha fecha2(25, 12, 2020);
+    Fecha fecha1(1, 1, 2022);
+    Fecha diaDelProgramador(13, 9, 2022);
 
-    int dif = fecha1.difEnDias(&fecha2);
-    int dif2 = fecha1 - &fecha2;
+    int dif = fecha1 - diaDelProgramador;
 
-    Fecha fSuma = fecha1.sumarDias(60);
+    cout << "Diferencia en dias (Debe ser -255): " << dif << endl;
+
     Fecha fSuma2 = fecha1 + 60;
 
+    fecha1.setDMA(1, 8, 2022);
+
+    cout << "El día del programador es: " << diaDelProgramador << endl;
+
+    cout << "Posincremento (13/9/2022): " << diaDelProgramador++ << endl;
+    cout << "Preincremento (15/9/2022): " << ++diaDelProgramador << endl;
 
     return 0;
 }
